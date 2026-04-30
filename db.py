a2d2da754027447590bb290f2d761d45ec2d9ad2
mckinsta <morekoushal@gmail.com>
2026-04-30 20:59:11 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,8 +1,10 @@
 import sqlite3
 
+# ================= DB CONNECTION =================
 conn = sqlite3.connect("movies.db", check_same_thread=False)
 cur = conn.cursor()
 
+# ================= TABLE =================
 cur.execute("""
 CREATE TABLE IF NOT EXISTS movies (
     name TEXT,
@@ -11,23 +13,34 @@ CREATE TABLE IF NOT EXISTS movies (
 )
 """)
 
+cur.execute("CREATE INDEX IF NOT EXISTS idx_name ON movies(name)")
 conn.commit()
 
-
+# ================= ADD MOVIE =================
 def add_movie(name, part, file_id):
-    name = name.lower().strip().replace(".mp4","")
-    cur.execute("INSERT INTO movies VALUES (?, ?, ?)", (name, part, file_id))
+    name = name.lower().strip().replace(".mp4", "")
+
+    cur.execute(
+        "INSERT INTO movies (name, part, file_id) VALUES (?, ?, ?)",
+        (name, part, file_id)
+    )
     conn.commit()
 
-
+# ================= GET PARTS =================
 def get_parts(name):
     name = name.lower().strip()
+
     cur.execute("SELECT part FROM movies WHERE name=?", (name,))
     return [i[0] for i in cur.fetchall()]
 
-
+# ================= GET MOVIE =================
 def get_movie(name, part):
     name = name.lower().strip()
-    cur.execute("SELECT file_id FROM movies WHERE name=? AND part=?", (name, part))
+
+    cur.execute(
+        "SELECT file_id FROM movies WHERE name=? AND part=?",
+        (name, part)
+    )
+
     data = cur.fetchone()
     return data[0] if data else None
